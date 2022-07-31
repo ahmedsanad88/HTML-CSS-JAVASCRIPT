4704d04256589e6577a13f06facc8e8f04cb80d1
ahmedsanad88 <57454543+ahmedsanad88@users.noreply.github.com>
2022-07-31 18:39:40 +0200

    upload nav folder for creating float navbar

---
+++ nav/app.js
@@ -0,0 +1,39 @@
+// show and hide the navbar.
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const nav = document.getElementById("navMove");
+
+const listItems = document.querySelectorAll("li");
+
+open.addEventListener("click", function () {
+  this.classList.add("animate__open");
+  close.classList.remove("animate__close");
+  nav.classList.add("nav__move");
+  nav.style.width = "50%";
+  setTimeout(() => {
+    listItems.forEach((item) => (item.style.opacity = 1));
+  }, 1000);
+});
+
+close.addEventListener("click", function () {
+  listItems.forEach((item) => (item.style.opacity = 0));
+  nav.style.width = "5%";
+  this.classList.add("animate__close");
+  open.classList.remove("animate__open");
+  setTimeout(() => {
+    nav.classList.remove("nav__move");
+  }, 1000);
+});
+
+// handle the active section
+
+listItems.forEach((item) => {
+  item.addEventListener("click", handleActiveSection);
+});
+
+function handleActiveSection(e) {
+  listItems.forEach((item) => item.classList.remove("active"));
+  e.target.classList.add("active");
+}
+
+// Can call the same function ->  handleActiveSection to keep the reference to selected section.
